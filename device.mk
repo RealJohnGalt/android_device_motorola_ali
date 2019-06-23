92d2014a56079bb3572fb915d93c8d7994221bc5
RealJohnGalt <johngalt@fake.mail>
2019-06-22 21:35:21 -0700

    tetheroffload
    Change-Id: I008ad52699792d6fec9b7686c781a6939d94b589

--- device.mk
+++ device.mk
@@ -123,6 +123,7 @@ PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
     android.hardware.wifi.supplicant@1.0 \
     android.hardware.wifi.supplicant@1.1 \
+    android.hardware.tetheroffload.control@1.0 \
     android.hardware.radio.config@1.0 \
     android.hardware.radio@1.0 \
     android.hardware.radio@1.1 \
